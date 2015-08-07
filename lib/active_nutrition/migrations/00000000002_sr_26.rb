5922d5dd2c2428021f6b02870b915b2257a40fab
Ikenna Okpala <me@ikennaokpala.com>
2015-08-07 11:43:56 +0100

    Define r26 migration and use it as download source

--- lib/active_nutrition/migrations/00000000002_sr_26.rb
+++ lib/active_nutrition/migrations/00000000002_sr_26.rb
@@ -2,7 +2,7 @@
 
 module ActiveNutrition
   module Migrations
-    class Sr24 < ActiveRecord::Migration
+    class Sr26 < ActiveRecord::Migration
       def self.up
         create_table "abbrev", :primary_key => "NDB_No", :force => true do |t|
           t.string  "Shrt_Desc",                  :limit => 60
